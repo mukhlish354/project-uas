ebc1176a1440f4431b96cbe3b3ffd6cb50e90deb
mukhlish354 <mukhlishabdulaziz@gmail.com>
2025-01-09 11:02:32 +0700

    Delete uas directory

--- uas/classstudent.py
+++
@@ -1,6 +0,0 @@
-class Student:
-    """Class untuk menyimpan data mahasiswa."""
-    def __init__(self, name, age, major):
-        self.name = name
-        self.age = age
-        self.major = major
